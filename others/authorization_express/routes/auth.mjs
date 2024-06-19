b2fbb4484df6b8c6d716e95bbf66ed2b71ce56a6
Daniel Nager Carpio <danielnagercarpios@gmail.com>
2024-06-19 15:40:39 +0200

    feat: added some files to Authorization
    User.mjs -> Schema to save data in mongoose
    Auth.mjs -> Routes for auth

---
+++ others/authorization_express/routes/auth.mjs
@@ -0,0 +1,40 @@
+import express from 'express'
+import jsonwebtoken from 'jsonwebtoken'
+import User from '../models/User.mjs'
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+	try {
+		const { username, password } = req.body;
+		const user = new User({username, password});
+		await user.save();
+		res.status(200).json({message: "New user registered successfully"})
+	} catch (error) {
+		res.status(500).json({message: "Internal server error"})
+	}
+})
+
+router.post('/login', async (req, res) => {
+	const { username, password } = req.body;
+	try {
+		const user = await User.findOne({username})
+		if (!user) {
+			return res.status(401).json({message: "Invalid username or password"});
+		}
+		if(user.password !== password) {
+			return res.status(401).json({message: "Invalid username or password"});
+		}
+		// Generate JWT token
+		const token = jwt.sign(
+			{id: user._id, username: user.username},
+			process.env.JWT_SECRET
+		)
+		res.json({ token });
+	} catch (error) {
+		res.status(500).json({ message: "Internal server error"});
+	}
+
+})
+
+export default router
